096be73e46f18d5eb93308ce867e95000b87dec7
fuadmmnf <bsse0920@iit.du.ac.bd>
2023-10-06 02:07:50 +0600

    added photo save in user update

--- app/Containers/AppSection/User/Actions/UpdateUserAction.php
+++ app/Containers/AppSection/User/Actions/UpdateUserAction.php
@@ -9,12 +9,14 @@ use App\Containers\AppSection\User\UI\API\Requests\UpdateUserRequest;
 use App\Ship\Exceptions\NotFoundException;
 use App\Ship\Exceptions\UpdateResourceFailedException;
 use App\Ship\Parents\Actions\Action as ParentAction;
+use Illuminate\Support\Facades\Storage;
 
 class UpdateUserAction extends ParentAction
 {
     public function __construct(
         private readonly UpdateUserTask $updateUserTask
-    ) {
+    )
+    {
     }
 
     /**
@@ -25,12 +27,33 @@ class UpdateUserAction extends ParentAction
     public function run(UpdateUserRequest $request): User
     {
         $sanitizedData = $request->sanitizeInput([
+            'mobile',
+            'email',
             'password',
             'name',
-            'gender',
-            'birth',
+            'name_bangla',
+            'designation',
+            'commissionerate',
+            'division',
+            'circle',
+            'address',
+//            'gender' => 'in:male,female,unspecified',
+            'dob',
+            'joining_date',
+            'fee_collection_start',
+            'is_verified'
         ]);
+        if ($request->hasFile('photo')) {
+            $image = $request->file('photo');
+            $image_uploaded_path = $image->store("images/members", 'public');
+            $uploadedImageResponse = array(
+                "image_name" => basename($image_uploaded_path),
+                "image_url" => Storage::disk('public')->url($image_uploaded_path),
+                "mime" => $image->getClientMimeType()
+            );
 
+            $sanitizedData['photo'] = $uploadedImageResponse['image_url'];
+        }
         return $this->updateUserTask->run($sanitizedData, $request->id);
     }
 }
