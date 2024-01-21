2540ff2bbff3d0b7f0b8f5cde1cb91a35eea3bbf
fuadmmnf <bsse0920@iit.du.ac.bd>
2024-01-21 11:50:29 +0600

    update candidate photo typo fix

--- app/Containers/AppSection/ElectionCandidate/UI/API/Controllers/UpdateCandidatePhotoController.php
+++ app/Containers/AppSection/ElectionCandidate/UI/API/Controllers/UpdateCandidatePhotoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Containers\AppSection\ElectionCandidate\UI\API\Controllers;
 
-use App\Containers\AppSection\ElectionCandidate\Actions\UpdateCommitteeMemberPhotoAction;
+use App\Containers\AppSection\ElectionCandidate\Actions\UpdateCandidatePhotoAction;
 use App\Containers\AppSection\ElectionCandidate\UI\API\Requests\UpdateCandidatePhotoRequest;
 use App\Ship\Parents\Controllers\ApiController;
 
@@ -10,7 +10,7 @@ class UpdateCandidatePhotoController extends ApiController
 {
     public function __invoke(UpdateCandidatePhotoRequest $request): \Illuminate\Http\JsonResponse
     {
-        $candidate = app(UpdateCommitteeMemberPhotoAction::class)->run($request);
+        $candidate = app(UpdateCandidatePhotoAction::class)->run($request);
 
         return $this->noContent(status: $candidate? 204: 400);
     }
